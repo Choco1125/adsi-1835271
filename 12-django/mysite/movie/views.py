b20ce1b853a9dec42028e360040ee3c2f3564a27
choco1125 <sebascruz981125@gmail.com>
2020-08-26 15:22:15 -0500

    Se lista por categorias, se organiza el home y se aplica paginacion y estilos

--- 12-django/mysite/movie/views.py
+++ 12-django/mysite/movie/views.py
@@ -3,8 +3,20 @@ from django.views.generic import ListView, DetailView
 from .models import Movie
 
 # Create your views here.
+class HomeView(ListView):
+  model = Movie
+  template_name = 'movie/home.html'
+
+  def get_context_data(self, **kwargs):
+    context = super(HomeView, self).get_context_data(**kwargs)
+    context['top_rated'] = Movie.objects.filter(status = 'TR')
+    context['most_watched'] = Movie.objects.filter(status = 'MW')
+    context['recently_added'] = Movie.objects.filter(status = 'RA')
+    return context
+
 class MovieList(ListView):
   model = Movie
+  paginate_by = 4
 
 class MovieDetail(DetailView):
   model = Movie
